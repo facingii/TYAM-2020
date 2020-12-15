506a22ab4917400f78092e1a131a6395dd1d8684
unknown <Gonzalo@CASSIOPEIA.veracruz.intra.uv.mx>
2020-12-15 14:01:12 -0600

    Fling Animation

--- AnimViewApp/app/src/main/java/mx/uv/fiee/iinf/animviewapp/MainActivity.java
+++ AnimViewApp/app/src/main/java/mx/uv/fiee/iinf/animviewapp/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +24,21 @@ public class MainActivity extends Activity {
         EditText edtFahrentheit = findViewById (R.id.edtFahrenheit);
         EditText edtCelsius = findViewById (R.id.edtCelsius);
 
+        Button btnJumping = findViewById (R.id.btnJumping);
+        SpringAnimation springAnimation = new SpringAnimation (btnJumping, DynamicAnimation.Y);
+
+        SpringForce springForce = new SpringForce ();
+        springForce.setFinalPosition (btnJumping.getScaleY () );
+        springForce.setDampingRatio (SpringForce.DAMPING_RATIO_LOW_BOUNCY);
+        springForce.setStiffness (SpringForce.STIFFNESS_LOW);
+
+        springAnimation.setSpring (springForce);
+
+        btnJumping.setOnClickListener (view -> {
+            springAnimation.setStartValue (2000f);
+            springAnimation.start ();
+        });
+
         Animation moveView = AnimationUtils.loadAnimation (getBaseContext (), R.anim.move);
         moveView.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -27,7 +46,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                double foo = Double.valueOf (edtFahrentheit.getText ().toString ()) * 100;
+                double foo = Double.parseDouble (edtFahrentheit.getText ().toString ()) * 100;
                 edtCelsius.setText (String.valueOf (foo));
             }
 
